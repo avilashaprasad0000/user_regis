fd4488ccef9836756fe75d9ee94ce22f9b2daa46
Pranav <pranavunnikrishnan@gmail.com>
2019-12-16 12:34:48 +0530

    Added Nav Drawer

--- app/src/main/java/com/example/user_regis/MainActivity.java
+++ app/src/main/java/com/example/user_regis/MainActivity.java
@@ -21,8 +21,8 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et1,et2;
-    Button b1,b2;
+    EditText et1, et2;
+    Button b1, b2, test;
     FirebaseAuth firebaseAuth;
     ProgressDialog pd;
     String email;
@@ -32,92 +32,103 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firebaseAuth= FirebaseAuth.getInstance();
-        et1=findViewById(R.id.et1);
-        et2=findViewById(R.id.et2);
-        pd= new ProgressDialog(MainActivity.this);
-        b1=findViewById(R.id.button);
-        b2=findViewById(R.id.button2);
+        firebaseAuth = FirebaseAuth.getInstance();
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        pd = new ProgressDialog(MainActivity.this);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        test = findViewById(R.id.test);
 
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(MainActivity.this, HomeActivity.class);
+                Toast.makeText(MainActivity.this, "Loading Home... ", Toast.LENGTH_SHORT).show();
+                startActivity(go);
+            }
+        });
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                email=et1.getText().toString().trim();
-                password=et2.getText().toString().trim();
-                if(TextUtils.isEmpty(email))
-                {
-                    Toast.makeText(MainActivity.this," Please enter email",Toast.LENGTH_SHORT).show();
+                email = et1.getText().toString().trim();
+                password = et2.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(MainActivity.this, " Please enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(TextUtils.isEmpty(password))
-                {
-                    Toast.makeText(MainActivity.this," Please enter password",Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, " Please enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                pd.setMessage("Registrating User ....");
+                pd.setMessage("Signing you up...");
                 pd.show();
-                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(MainActivity.this, "Registration is successful ! ", Toast.LENGTH_SHORT).show();
-                            Intent avi=new Intent(MainActivity.this,profile.class);
+                            Intent avi = new Intent(MainActivity.this, HomeActivity.class);
                             avi.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             startActivity(avi);
                             pd.dismiss();
                             //startActivity(new Intent (MainActivity.this,loginact.class));
-                        }
-                        else {
+                        } else {
                             if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                                 Toast.makeText(MainActivity.this, "You are already registered", Toast.LENGTH_SHORT).show();
 
+                            } else {
+                                Toast.makeText(MainActivity.this, "Unable to register you, please try again ! ", Toast.LENGTH_SHORT).show();
+
                             }
-                            else
-                            { Toast.makeText(MainActivity.this, "Could not registration, please try again ! ", Toast.LENGTH_SHORT).show();
-
-                        }
                             pd.dismiss();
 
-                }
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email=et1.getText().toString().trim();
-                password=et2.getText().toString().trim();
-                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        }
+                    }
+                });
+                b2.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            finish();
-                            Intent avi=new Intent(MainActivity.this,profile.class);
-                            avi.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(avi);
-                            //FirebaseUser user = firebaseAuth.getCurrentUser();
+                    public void onClick(View v) {
+                        email = et1.getText().toString().trim();
+                        password = et2.getText().toString().trim();
+                        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    finish();
+                                    Intent avi = new Intent(MainActivity.this, HomeActivity.class);
+                                    avi.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(avi);
+                                    //FirebaseUser user = firebaseAuth.getCurrentUser();
 
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "Invalid Credentials, please try again ! ", Toast.LENGTH_SHORT).show();
-                            pd.dismiss();
-                        }
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Invalid Credentials, please try again ! ", Toast.LENGTH_SHORT).show();
+                                    pd.dismiss();
+                                }
+
+                            }
+                        });
 
                     }
                 });
 
+
             }
         });
     }
-});}@Override
+
+    @Override
     protected void onStart() {
         super.onStart();
 
         if (firebaseAuth.getCurrentUser() != null) {
             finish();
-            startActivity(new Intent(this, profile.class));
+            startActivity(new Intent(this, HomeActivity.class));
         }
-    }}
+    }
+}
 
 
